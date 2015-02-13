bee0b633409390f279ebf69a96218290d5ec3722
rchavarria <txingo@gmail.com>
2015-02-13 21:42:49 +0100

    Include gulpfile.js in the jslint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
-var gulp = require('gulp'),
-    mocha = require('gulp-mocha'),
-    jslint = require('gulp-jslint');
+(function () {
+    'use strict';
 
-gulp.task('jslint', function () {
-    return gulp
-        .src(['src/scripts/**/*.js', 'test/scripts/**/*.js'])
-        .pipe(jslint({
-            global: ['module', 'require', 'describe', 'it', 'expect']
-        }));
-});
+    var gulp = require('gulp'),
+        mocha = require('gulp-mocha'),
+        jslint = require('gulp-jslint');
 
-gulp.task('test', ['jslint'], function () {
-    return gulp
-        .src(['test/bootstrap.js', 'test/scripts/**/*.js'])
-        .pipe(mocha({ reporter: 'spec' }));
-});
+    gulp.task('jslint', function () {
+        return gulp
+            .src(['gulpfile.js', 'src/scripts/**/*.js', 'test/scripts/**/*.js'])
+            .pipe(jslint({
+                global: ['module', 'require', 'describe', 'it', 'expect']
+            }));
+    });
 
-gulp.task('test-watch', function () {
-    return gulp.watch(['src/scripts/**/*.js', 'test/scripts/**/*.js'], ['test']);
-});
+    gulp.task('test', ['jslint'], function () {
+        return gulp
+            .src(['test/bootstrap.js', 'test/scripts/**/*.js'])
+            .pipe(mocha({ reporter: 'spec' }));
+    });
+
+    gulp.task('test-watch', function () {
+        return gulp.watch(['src/scripts/**/*.js', 'test/scripts/**/*.js'], ['test']);
+    });
+}());
 
