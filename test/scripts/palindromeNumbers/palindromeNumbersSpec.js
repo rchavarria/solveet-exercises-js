90e260f11b2bb7d0b7db7da2f42b26a1fbe99ee8
rchavarria <txingo@gmail.com>
2015-02-14 16:56:17 +0100

    Refactor : improve name

--- test/scripts/palindromeNumbers/palindromeNumbersSpec.js
+++ test/scripts/palindromeNumbers/palindromeNumbersSpec.js
@@ -7,18 +7,18 @@ describe('Palindrome Numbers', function () {
     describe('#sumIsPalindrome', function () {
 
         it('the sum of a number and its reverse is palindrome', function () {
-            var p = new Palindrome(11, 1);
-            expect(p.sumIsPalindrome()).equals(true);
+            var palindrome = new Palindrome(11, 1);
+            expect(palindrome.sumIsPalindrome()).equals(true);
         });
 
         it('the sum of a number and its reverse is NOT palindrome at first iteration', function () {
-            var p = new Palindrome(48, 1);
-            expect(p.sumIsPalindrome()).equals(false);
+            var palindrome = new Palindrome(48, 1);
+            expect(palindrome.sumIsPalindrome()).equals(false);
         });
 
         it('the sum of a number and its reverse after some iterations', function () {
-            var p = new Palindrome(48, 2);
-            expect(p.sumIsPalindrome()).equals(true);
+            var palindrome = new Palindrome(48, 2);
+            expect(palindrome.sumIsPalindrome()).equals(true);
         });
 
     });
