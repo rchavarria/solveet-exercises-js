d8a9ae1bebd47e1975343c29372a9a3bd3f0e8bc
rchavarria <txingo@gmail.com>
2015-02-17 09:13:46 +0100

    Change from JSLint to JSHint to spell check my .js files. JSHint support ES6 modules syntax

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,26 @@
     var gulp = require('gulp'),
         mocha = require('gulp-spawn-mocha'),
         jslint = require('gulp-jslint'),
+        jshint = require('gulp-jshint'),
         babel = require('gulp-babel');
 
     gulp.task('jslint', function () {
         return gulp
             .src(['gulpfile.js', 'src/scripts/**/*.js', 'test/scripts/**/*.js'])
             .pipe(jslint({
-                global: ['console', 'module', 'require', 'describe', 'it', 'expect']
+                global: ['console', 'module', 'require', 'describe', 'it', 'expect'],
+                es6: true
             }));
+    });
+
+    gulp.task('jshint', function () {
+        return gulp
+            .src(['gulpfile.js', 'src/scripts/**/*.js', 'test/scripts/**/*.js'])
+            .pipe(jshint({
+                globals: ['console', 'module', 'require', 'describe', 'it', 'expect'],
+                esnext: true
+            }))
+            .pipe(jshint.reporter('jshint-stylish'));
     });
 
     gulp.task('test', function () {
