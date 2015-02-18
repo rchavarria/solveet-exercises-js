cc9dbb85b77a57ebe3fb7a9b80055c46016a9ca0
rchavarria <txingo@gmail.com>
2015-02-18 17:48:16 +0100

    Use class in the production folder

--- test/scripts/football-tshirts/footballTShirtsSpec.js
+++ test/scripts/football-tshirts/footballTShirtsSpec.js
@@ -1,28 +1,4 @@
-class TShirts {
-
-    getJoinedSquares() {
-        var joined = [];
-
-        for(let i = 0; i < 100; i++) {
-            for (let j = 0; j < 100; j++) {
-                let square = Math.pow(i + j, 2).toString();
-                let paddedI = this.pad(i);
-                let paddedJ = this.pad(j);
-
-                if (square === (paddedI + paddedJ)) {
-                    joined.push([paddedI, paddedJ, square]);
-                }
-            }
-        }
-
-        return joined;
-    }
-
-    pad(n) {
-        return ('00' + n).slice(-2);
-    }
-
-}
+import TShirts from '../../../src/scripts/football-tshirts/footballTShirts.js';
 
 describe('Football t-shirts', () => {
 
