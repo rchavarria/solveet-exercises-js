c77a4ea51cd687d9892549cb7c76bd87d1e3714e
rchavarria <txingo@gmail.com>
2015-02-18 17:27:37 +0100

    Create a class and move the solution to that class

--- test/scripts/football-tshirts/footballTShirtsSpec.js
+++ test/scripts/football-tshirts/footballTShirtsSpec.js
@@ -1,3 +1,28 @@
+class TShirts {
+    getJoinedSquares() {
+        var joined = [];
+
+        for(let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                let square = Math.pow(i + j, 2).toString();
+                let paddedI = this.pad(i);
+                let paddedJ = this.pad(j);
+
+                if (square === (paddedI + paddedJ)) {
+                    joined.push([paddedI, paddedJ, square]);
+                }
+            }
+        }
+
+        return joined;
+    }
+
+    pad(n) {
+        return ('00' + n).slice(-2);
+    }
+
+}
+
 describe('Football t-shirts', () => {
 
     var pad = (n) => ('00' + n).slice(-2);
@@ -14,6 +39,17 @@ describe('Football t-shirts', () => {
                 }
             }
         }
+    });
+
+    describe('#getJoinedSquares', () => {
+
+        it('returns a list of lists with (X + Y)^2 == XY', () => {
+            var tshirts = new TShirts(),
+                joined = tshirts.getJoinedSquares();
+
+            expect(joined[0]).deep.equal(['20', '25', '2025']);
+        });
+
     });
 
 });
