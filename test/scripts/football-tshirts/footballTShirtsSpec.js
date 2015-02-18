8b8def4ce0a22fc25e846e2b0fedb5058b92923f
rchavarria <txingo@gmail.com>
2015-02-18 17:29:33 +0100

    Remove initial test, no longer needed

--- test/scripts/football-tshirts/footballTShirtsSpec.js
+++ test/scripts/football-tshirts/footballTShirtsSpec.js
@@ -1,4 +1,5 @@
 class TShirts {
+
     getJoinedSquares() {
         var joined = [];
 
@@ -24,22 +25,6 @@ class TShirts {
 }
 
 describe('Football t-shirts', () => {
-
-    var pad = (n) => ('00' + n).slice(-2);
-
-    it('prints out numbers where (X + Y)^2 == XY', () => {
-        for(let i = 0; i < 100; i++) {
-            for (let j = 0; j < 100; j++) {
-                let square = Math.pow(i + j, 2);
-                let paddedI = pad(i);
-                let paddedJ = pad(j);
-
-                if (square.toString() === (paddedI + paddedJ)) {
-                    console.log('(', paddedI, ' + ', paddedJ, ') == ', square);
-                }
-            }
-        }
-    });
 
     describe('#getJoinedSquares', () => {
 
