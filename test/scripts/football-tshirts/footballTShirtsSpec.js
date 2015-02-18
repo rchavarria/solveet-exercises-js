ddfac59b42a777acaf27ebf5a3d517e496786da9
rchavarria <txingo@gmail.com>
2015-02-18 09:08:19 +0100

    Refactor : make it more ES6-ish by using 'let' inside some code blocks

--- test/scripts/football-tshirts/footballTShirtsSpec.js
+++ test/scripts/football-tshirts/footballTShirtsSpec.js
@@ -1,14 +1,13 @@
 describe('Football t-shirts', function () {
 
     it('prints out numbers where (X + Y)^2 == XY', function () {
-        var i, j, square, paddedI, paddedJ;
+        for(let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                let square = Math.pow(i + j, 2);
+                let paddedI = ('00' + i).slice(-2);
+                let paddedJ = ('00' + j).slice(-2);
 
-        for(i = 0; i < 100; i++) {
-            for (j = 0; j < 100; j++) {
-                square = Math.pow(i + j, 2);
-                paddedI = ('00' + i).slice(-2);
-                paddedJ = ('00' + j).slice(-2);
-                if (square.toString() === paddedI + paddedJ) {
+                if (square.toString() === (paddedI + paddedJ)) {
                     console.log('(', paddedI, ' + ', paddedJ, ') == ', square);
                 }
             }
