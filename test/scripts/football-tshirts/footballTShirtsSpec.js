592449e084393d20ebf6f1e5d4d3df292ecac6f1
rchavarria <txingo@gmail.com>
2015-02-18 09:14:23 +0100

    Remove duplicated code by extracting a method

--- test/scripts/football-tshirts/footballTShirtsSpec.js
+++ test/scripts/football-tshirts/footballTShirtsSpec.js
@@ -1,11 +1,13 @@
 describe('Football t-shirts', () => {
 
+    var pad = (n) => ('00' + n).slice(-2);
+
     it('prints out numbers where (X + Y)^2 == XY', () => {
         for(let i = 0; i < 100; i++) {
             for (let j = 0; j < 100; j++) {
                 let square = Math.pow(i + j, 2);
-                let paddedI = ('00' + i).slice(-2);
-                let paddedJ = ('00' + j).slice(-2);
+                let paddedI = pad(i);
+                let paddedJ = pad(j);
 
                 if (square.toString() === (paddedI + paddedJ)) {
                     console.log('(', paddedI, ' + ', paddedJ, ') == ', square);
