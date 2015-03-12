2f388fce6fbe489cd67bff2cc261df8c0f18defd
rchavarria <txingo@gmail.com>
2015-03-12 17:03:11 +0100

    Computes gcd for numbers that are primes between them, they have no common factor

--- test/scripts/simplify-fractions/simplifyFractionsSpec.js
+++ test/scripts/simplify-fractions/simplifyFractionsSpec.js
@@ -9,9 +9,11 @@ describe('Fraction', () => {
 
     describe('#gcd', () => {
 
-        it('is 1 for 1 and 1', () => {
-            let gcd = fraction.gcd(1, 1);
-            expect(gcd).equal(1);
+        it('is 1 for relative prime numbers', () => {
+            expect(fraction.gcd(1, 1)).equal(1);
+            expect(fraction.gcd(1, 2)).equal(1);
+            expect(fraction.gcd(2, 3)).equal(1);
+            expect(fraction.gcd(8, 27)).equal(1);
         });
 
     });
