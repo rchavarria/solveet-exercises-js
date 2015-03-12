d6981546910a5fab1185dfbaf354cb902fe9460d
rchavarria <txingo@gmail.com>
2015-03-12 16:53:06 +0100

    Start working on a new solveet exercise

---
+++ test/scripts/simplify-fractions/simplifyFractionsSpec.js
@@ -0,0 +1,9 @@
+import Fraction from '../../../src/scripts/simplify-fractions/simplifyFractions.js';
+
+describe('Fraction', () => {
+
+    it('initializes', () => {
+        expect(Fraction).not.to.equal(undefined);
+    });
+
+});
