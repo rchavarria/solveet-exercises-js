86f36fa45cd681a06f92a8c6d13ca4aa9658eef4
rchavarria <txingo@gmail.com>
2015-03-16 08:57:30 +0100

    Replace two 'expects' with a 'deep.equal'

--- test/scripts/simplify-fractions/simplifyFractionsSpec.js
+++ test/scripts/simplify-fractions/simplifyFractionsSpec.js
@@ -21,9 +21,7 @@ describe('Fraction', () => {
     describe('#simplify', () => {
 
         it('returns the same fraction if can not be reduced', () => {
-            let reduced = new Fraction(1, 2).simplify();
-            expect(reduced.numerator).equal(1);
-            expect(reduced.denominator).equal(2);
+            expect(new Fraction(1, 2).simplify()).deep.equal(new Fraction(1, 2));
         });
 
         it('simplifies by the common factor', () => {
