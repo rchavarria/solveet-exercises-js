c8421589aa4f5398b4958782e5b92343cd501a5b
rchavarria <txingo@gmail.com>
2015-03-17 09:46:14 +0100

    Remove obsolete code

--- src/scripts/simplify-fractions/simplifyFractions.js
+++ src/scripts/simplify-fractions/simplifyFractions.js
@@ -5,10 +5,6 @@ export default class Fraction {
         this.denominator = denominator;
     }
 
-    gcd(a, b) {
-        return gcd(a, b);
-    }
-
     simplify() {
         let divisor = gcd(this.numerator, this.denominator);
         return new Fraction(this.numerator / divisor, this.denominator / divisor);
