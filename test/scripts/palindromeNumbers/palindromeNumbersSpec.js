29cb5d0925ac3fd00470cc611a98a7d6a6ba6a20
rchavarria <txingo@gmail.com>
2015-03-24 09:29:53 +0100

    Refactor: inline some variables

--- test/scripts/palindromeNumbers/palindromeNumbersSpec.js
+++ test/scripts/palindromeNumbers/palindromeNumbersSpec.js
@@ -5,18 +5,15 @@ describe('Palindrome Numbers', () => {
     describe('#sumIsPalindrome', () => {
 
         it('the sum of a number and its reverse is palindrome', () => {
-            var palindrome = new Palindrome(11, 1);
-            expect(palindrome.sumIsPalindrome()).equals(true);
+            expect(new Palindrome(11, 1).sumIsPalindrome()).equals(true);
         });
 
         it('the sum of a number and its reverse is NOT palindrome at first iteration', () => {
-            var palindrome = new Palindrome(48, 1);
-            expect(palindrome.sumIsPalindrome()).equals(false);
+            expect(new Palindrome(48, 1).sumIsPalindrome()).equals(false);
         });
 
         it('the sum of a number and its reverse after some iterations', () => {
-            var palindrome = new Palindrome(48, 2);
-            expect(palindrome.sumIsPalindrome()).equals(true);
+            expect(new Palindrome(48, 2).sumIsPalindrome()).equals(true);
         });
 
     });
@@ -24,10 +21,8 @@ describe('Palindrome Numbers', () => {
     describe('Specific examples', () => {
 
         it('187 needs at least 23 iterations', () => {
-            var palindrome = new Palindrome(187, 22);
-            expect(palindrome.sumIsPalindrome()).equals(false);
-            palindrome = new Palindrome(187, 23);
-            expect(palindrome.sumIsPalindrome()).equals(true);
+            expect(new Palindrome(187, 22).sumIsPalindrome()).equals(false);
+            expect(new Palindrome(187, 23).sumIsPalindrome()).equals(true);
         });
 
     });
