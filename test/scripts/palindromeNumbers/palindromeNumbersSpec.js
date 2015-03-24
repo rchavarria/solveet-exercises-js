af3c162eeea7ab45b7af721c338d19acb32676d9
rchavarria <txingo@gmail.com>
2015-03-24 09:24:20 +0100

    Use arrow functions in tests

--- test/scripts/palindromeNumbers/palindromeNumbersSpec.js
+++ test/scripts/palindromeNumbers/palindromeNumbersSpec.js
@@ -1,29 +1,29 @@
 import Palindrome from '../../../src/scripts/palindromeNumbers/palindromeNumbers.js';
 
-describe('Palindrome Numbers', function () {
+describe('Palindrome Numbers', () => {
 
-    describe('#sumIsPalindrome', function () {
+    describe('#sumIsPalindrome', () => {
 
-        it('the sum of a number and its reverse is palindrome', function () {
+        it('the sum of a number and its reverse is palindrome', () => {
             var palindrome = new Palindrome(11, 1);
             expect(palindrome.sumIsPalindrome()).equals(true);
         });
 
-        it('the sum of a number and its reverse is NOT palindrome at first iteration', function () {
+        it('the sum of a number and its reverse is NOT palindrome at first iteration', () => {
             var palindrome = new Palindrome(48, 1);
             expect(palindrome.sumIsPalindrome()).equals(false);
         });
 
-        it('the sum of a number and its reverse after some iterations', function () {
+        it('the sum of a number and its reverse after some iterations', () => {
             var palindrome = new Palindrome(48, 2);
             expect(palindrome.sumIsPalindrome()).equals(true);
         });
 
     });
 
-    describe('Specific examples', function () {
+    describe('Specific examples', () => {
 
-        it('187 needs at least 23 iterations', function () {
+        it('187 needs at least 23 iterations', () => {
             var palindrome = new Palindrome(187, 22);
             expect(palindrome.sumIsPalindrome()).equals(false);
             palindrome = new Palindrome(187, 23);
