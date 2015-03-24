fc6508d8703ab14826967384fe5ecc6f6cbe0f1e
rchavarria <txingo@gmail.com>
2015-03-24 09:26:53 +0100

    Move isPalindrome() method outside the class, to make it private to the module instead of being a public method in the class

--- src/scripts/palindromeNumbers/palindromeNumbers.js
+++ src/scripts/palindromeNumbers/palindromeNumbers.js
@@ -3,16 +3,16 @@ function reverse(n) {
     return parseInt(n.toString().split('').reverse().join(''), 10);
 }
 
+function isPalindrome(n) {
+    return n.toString() === reverse(n).toString();
+}
+
 export default class Palindrome {
 
     constructor(candidateNumber, maxNumberOfIterations) {
         this.number = candidateNumber;
         this.maxIterations = maxNumberOfIterations;
         this.currentIteration = 0;
-    }
-
-    isPalindrome(n) {
-        return n.toString() === reverse(n).toString();
     }
 
     sumIsPalindrome() {
@@ -22,7 +22,7 @@ export default class Palindrome {
         }
 
         this.number += reverse(this.number);
-        if (this.isPalindrome(this.number)) {
+        if (isPalindrome(this.number)) {
             return true;
         }
         return this.sumIsPalindrome();
