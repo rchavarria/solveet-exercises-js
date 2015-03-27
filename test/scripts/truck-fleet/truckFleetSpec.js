888db6cf7a545473b6f85c189a61db253b3a626b
rchavarria <txingo@gmail.com>
2015-03-27 09:00:08 +0100

    Add test to check that a number does not end with a suffix

--- test/scripts/truck-fleet/truckFleetSpec.js
+++ test/scripts/truck-fleet/truckFleetSpec.js
@@ -1,9 +1,21 @@
+function endsWith(number, suffix) {
+    return false;
+}
+
 describe('Truck Fleet', () => {
 
     describe('hook', () => {
 
         it('runs test', () => {
             expect(1).equal(2 - 1);
+        });
+
+    });
+
+    describe('#endsWith', () => {
+
+        it('returns false when a number does not end with the given suffix', () => {
+            expect(endsWith(10, 1)).equal(false);
         });
 
     });
