8559a5f2ed82d915b0119982d125a922b1e40451
rchavarria <txingo@gmail.com>
2015-04-07 09:11:38 +0200

    Migrate all code to use the TruckNumber class

--- test/scripts/truck-fleet/truckFleetSpec.js
+++ test/scripts/truck-fleet/truckFleetSpec.js
@@ -1,32 +1,27 @@
 import TruckNumber from '../../../src/scripts/truck-fleet/truckFleet.js';
 
-function endsWith(number, suffix) {
-    let n = new TruckNumber(number);
-    return n.endsWith(suffix);
-}
-
 describe('Truck Fleet', () => {
 
     describe('#endsWith', () => {
 
         it('returns false when a number does not end with a one digit suffix', () => {
-            expect(endsWith(10, 1)).equal(false);
+            expect(new TruckNumber(10).endsWith(1)).equal(false);
         });
 
         it('returns true when a number ends with a one digit suffix', () => {
-            expect(endsWith(10, 0)).equal(true);
+            expect(new TruckNumber(10).endsWith(0)).equal(true);
         });
 
         it('returns false when Number does not end with a several digits suffix', () => {
-            expect(endsWith(12345, 10)).equal(false);
-            expect(endsWith(12345, 123)).equal(false);
-            expect(endsWith(12345, 1234)).equal(false);
+            expect(new TruckNumber(12345).endsWith(10)).equal(false);
+            expect(new TruckNumber(12345).endsWith(123)).equal(false);
+            expect(new TruckNumber(12345).endsWith(1234)).equal(false);
         });
 
         it('returns true when Number ends with a several digits suffix', () => {
-            expect(endsWith(12345, 45)).equal(true);
-            expect(endsWith(12345, 345)).equal(true);
-            expect(endsWith(12345, 2345)).equal(true);
+            expect(new TruckNumber(12345).endsWith(45)).equal(true);
+            expect(new TruckNumber(12345).endsWith(345)).equal(true);
+            expect(new TruckNumber(12345).endsWith(2345)).equal(true);
         });
 
     });
