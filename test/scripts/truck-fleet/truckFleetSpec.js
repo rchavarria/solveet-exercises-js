5da991dc35a7259f133612c54bdbb7609d3727e3
rchavarria <txingo@gmail.com>
2015-04-08 09:26:07 +0200

    Write a test as if it were the application to find all allowed truck numbers

--- test/scripts/truck-fleet/truckFleetSpec.js
+++ test/scripts/truck-fleet/truckFleetSpec.js
@@ -30,6 +30,8 @@ describe('Truck Fleet', () => {
 
         it('is not allowed when it is greater than 500', () => {
             expect(new TruckNumber(501).isAllowed()).equal(false);
+            // TODO  Add a case when the square ends with the number but its
+            // greater than 500
         });
 
         it('is allowed when the square of the number ends with the number itself', () => {
@@ -40,6 +42,22 @@ describe('Truck Fleet', () => {
         it('is not allowed when the square does not end with the number itself', () => {
             expect(new TruckNumber(51).isAllowed()).equal(false);
             expect(new TruckNumber(101).isAllowed()).equal(false);
+        });
+
+    });
+
+    describe('Find truck numbers', () => {
+
+        it('iterates from 1 to 500', () => {
+            let allowedNumbers = [];
+            for (let i = 1; i <= 500; i++) {
+                let allowed = new TruckNumber(i).isAllowed();
+                if (allowed) {
+                    allowedNumbers.push(i);
+                }
+            }
+
+            console.log('Allowed numbers for trucks:', allowedNumbers);
         });
 
     });
